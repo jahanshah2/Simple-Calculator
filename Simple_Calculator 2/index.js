9b94da858d2e31e8ff34f9f71b0d23fbc124c5f5
Sayed Jahan Shah <159986782+jahanshah2@users.noreply.github.com>
2024-03-17 12:25:01 +0500

    Add files via upload

---
+++ Simple_Calculator 2/index.js
@@ -0,0 +1,44 @@
+
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter The First Number", type: "number", name: "firstNumber" },
+    { message: "Enter The Second Number", type: "number", name: "secondNumber" },
+    {
+        message: "select the one operator",
+        type: "list",
+        name: "operators",
+        choices: [
+            "Addition",
+            "Subtrection",
+            "Multiplication",
+            "Division",
+            "Exponentiation",
+            "Modulus",
+            "BMI",
+        ],
+    },
+]);
+if (answer.operators === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operators === "Subtrection") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operators === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operators === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else if (answer.operators === "Exponentiation") {
+    console.log(answer.firstNumber ** answer.secondNumber);
+}
+else if (answer.operators === "Modulus") {
+    console.log(answer.firstNumber % answer.secondNumber);
+}
+else if (answer.operators === "BMI") {
+    console.log(answer.firstNumber / (answer.secondNumber * answer.secondNumber));
+}
+else {
+    console.log("Please Select Valid Operaters");
+}
